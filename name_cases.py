1db661b8bbd9532e68f9b7b8ea50f6737edd7509
Esther-Omolara <adewumiestheromolara@gmail.com>
2024-01-10 04:09:43 +0100

    practice

---
+++ name_cases.py
@@ -0,0 +1,9 @@
+name = 'Vincent,'
+print ('Hello' + " " + name + 'would you like to learn some python today?')
+name = 'adesewa omooba'
+print (name.lower(), name.upper(), name.title())
+author = 'Albert Einstein'
+quote = '"A person who never made a mistake never tried anything new."'
+message = (author + " " + 'once said,' + " " + quote)
+print (message)
+
